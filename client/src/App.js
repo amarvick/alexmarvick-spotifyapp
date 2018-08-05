4e698b0d394d6f338ec4579d89a16b79a6170d0c
Alex Marvick <alex@fiduciarytech.com>
2018-08-04 18:04:44 -0700

    Able to post playlist and play it (dealing with bug now)

--- client/src/App.js
+++ client/src/App.js
@@ -19,7 +19,8 @@ class App extends Component {
       accesstoken: token,
       loggedIn: token ? true : false,
       loggedInUser: {
-        userId: ''
+        userId: '',
+        userProduct: ''
       },
       nowPlaying: { 
         name: '', 
@@ -32,8 +33,10 @@ class App extends Component {
         artistId: ''
       },
       favoriteArtistsSongs: {
-        songs: []
-      }
+        songUris: []
+        // songNames: []
+      },
+      questionNumber: 1
     }
     console.log(params);
   }
@@ -49,32 +52,24 @@ class App extends Component {
     return hashParams;
   }
 
-  // Get logged in user
+  // Get logged in user + favorite artist.. Maybe combine the three functions used together?
   componentDidMount() {
-    spotifyApi.getMe()
-      .then((response) => {
-        this.setState({
-          loggedInUser: {
-            userId: response.id
-          }
-        })
-      })
+    this.getUser()
     this.getFavoriteArtist()
+    this.getFavoriteArtistsSongs(this.state.favoriteArtists.artistId, 'SE')
   }
 
-  // getNowPlaying() {
-  //   spotifyApi.getMyCurrentPlaybackState()
-  //     .then((response) => {
-  //       this.setState({
-  //         nowPlaying: {
-  //           name: response.item.name,
-  //           artist: response.item.artists[0].name,
-  //           type: response.item.type,
-  //           albumArt: response.item.album.images[0].url
-  //         }
-  //       })
-  //     })
-  // }
+  getUser() {
+    spotifyApi.getMe()
+    .then((response) => {
+      this.setState({
+        loggedInUser: {
+          userId: response.id,
+          userProduct: response.product
+        }
+      })
+    })
+  }
 
   // Get user's favorite artist
   getFavoriteArtist() {
@@ -86,7 +81,6 @@ class App extends Component {
             artistId: response.items[0].id
           }
         })
-        this.getFavoriteArtistsSongs(this.state.favoriteArtists.artistId, 'SE')
       })
   }
 
@@ -94,62 +88,149 @@ class App extends Component {
   getFavoriteArtistsSongs(artistId, US) {
     spotifyApi.getArtistTopTracks(artistId, US)
       .then((response) => {
-        var theSongs = []
+        var theSongUris = []
+        var theSongNames = []
         for (var i = 0; i < 10; i++) {
-          theSongs.push((i+1) + '. ' +response.tracks[i].name);
+          theSongUris.push(response.tracks[i].uri);
+          // theSongNames.push(response.tracks[i].name);
         }
+        this.shuffle(theSongUris)
         this.setState({
           favoriteArtistsSongs: {
-            songs: theSongs
+            songUris: theSongUris
           }
         })
-        // this.postPlaylist(this.state.loggedInUser.userId, theSongs)
       })
+  }
+
+  // Randomize the generated playlist order
+  shuffle(tracksArray) {
+    var currentIndex = tracksArray.length, temporaryValue, randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      // And swap it with the current element.
+      temporaryValue = tracksArray[currentIndex];
+      tracksArray[currentIndex] = tracksArray[randomIndex];
+      tracksArray[randomIndex] = temporaryValue;
+    }
+  
+    return tracksArray;
+  }
+
+
+  startGame() {
+    this.postPlaylist(this.state.loggedInUser.userId, this.state.favoriteArtistsSongs.songUris)
   }
 
   // Will create private playlist on user's spotify account
   postPlaylist(userId, allSongs) {
+    document.getElementById('modal').style.display = 'none'
+
     axios({
       url: 'https://api.spotify.com/v1/users/' + this.state.loggedInUser.userId + '/playlists',
       method: "POST",
       data: {
-        name: 'Game Playlist',
-        public: false
+        name: 'HOW BIG OF A ' + this.state.favoriteArtists.artist.toUpperCase() + ' FAN ARE YOU?',
+        public: true
       },
       headers: {
         'Authorization': 'Bearer ' + this.state.accesstoken,
         'Content-Type': 'application/json'
-      },
-      success: function(response) {
-        console.log(response);
-      }  
+      }
     })
       .then((response) => {
         var playlistId = response.data.id;
-        this.addTracksToPlaylist(response.data.id, allSongs);
+        this.addTracksToPlaylist(response.data.id, allSongs, response.data.uri);
       })
       .catch((error) => {
         alert('ERROR! ' + error)
+        console.log(error)
       })
   }
 
-  addTracksToPlaylist(newPlaylistId, allSongs) {
-    alert(newPlaylistId);
-    alert(allSongs);
+  // Then... add tracks initially received to newly created playlist
+  addTracksToPlaylist(newPlaylistId, allSongs, contextUri) {
+    axios({
+      url: 'https://api.spotify.com/v1/users/' + this.state.loggedInUser.userId + '/playlists/' + newPlaylistId + '/tracks/',
+      method: "POST",
+      data: {
+        uris: allSongs
+      },
+      headers: {
+        'Authorization': 'Bearer ' + this.state.accesstoken,
+        'Content-Type': 'application/json'
+      }
+    })
+      .then((response) => {
+        console.log(response);
+        this.playPlaylist(newPlaylistId, contextUri)
+      })
+      .catch((error) => {
+        alert(error)
+        console.log(error)
+      })
+
+  }
+
+  // Then... play the playlist to get started
+  playPlaylist(newPlaylistId, contextUri) {
+    axios({
+      url: 'https://api.spotify.com/v1/me/player/play',
+      method: "PUT",
+      data: {
+        context_uri: contextUri
+      },
+      headers: {
+        'Authorization': 'Bearer ' + this.state.accesstoken
+      }
+    })
+      .then((response) => {
+        console.log(response)
+      })
+      .catch((error) => {
+        alert(error)
+        console.log(error)
+      })
+    console.log('Will get now playing...')
+    this.generateQuestion()
+  }
+
+  generateQuestion() {
+    var options = [];
+  }
+
+  getNowPlaying() {
+    spotifyApi.getMyCurrentPlaybackState()
+      .then((response) => {
+        this.setState({
+          nowPlaying: {
+            name: response.item.name,
+            artist: response.item.artists[0].name,
+            type: response.item.type,
+            albumArt: response.item.album.images[0].url
+          }
+        })
+      })
   }
 
   render() {
-    let songs = [];
+    let songUris = [];
 
-    if (this.state.favoriteArtistsSongs.songs) {
-      songs = this.state.favoriteArtistsSongs.songs;
+    if (this.state.favoriteArtistsSongs.songUris) {
+      songUris = this.state.favoriteArtistsSongs.songUris;
     }
 
-    const orderedSongs = songs.map(song =>
-      <li>
-        {song}
-      </li>
-    )
+    // const orderedSongUris = songUris.map(songUri =>
+    //   <li>
+    //     {songUri}
+    //   </li>
+    // )
 
     return (
       <div className='App'>
@@ -157,10 +238,9 @@ class App extends Component {
           <a href='http://localhost:8888'> Login to Spotify </a>
         }
 
-        { this.state.loggedIn && this.state.nowPlaying.name !== '' && this.state.nowPlaying.name !== null &&
+        {/* { this.state.loggedIn && this.state.nowPlaying.name !== '' && this.state.nowPlaying.name !== null &&
           <div>
             { this.state.nowPlaying.artist } - { this.state.nowPlaying.name } <br/>
-            {/* Key: { this.state.nowPlaying.type } */}
           </div>
         }
 
@@ -168,7 +248,7 @@ class App extends Component {
           <div>
             <img src={ this.state.nowPlaying.albumArt } style={{ height: 150 }}/>
           </div>
-        }
+        } */}
 
         { this.state.loggedIn &&
           <div>
@@ -176,24 +256,46 @@ class App extends Component {
           </div>
         }
 
-        { this.state.loggedIn &&
+        {/* { this.state.loggedIn &&
           <div>
             Top Songs: <br/>
-            <ul>{orderedSongs}</ul>
+            <ul>{orderedSongUris}</ul>
           </div>
-        }
+        } */}
 
-        { this.state.loggedIn &&
+        {/* { this.state.loggedIn &&
           <button onClick={() => this.getNowPlaying()}>
             Check Now Playing
           </button>
+        } */}
+
+        { this.state.loggedIn && this.state.loggedInUser.userProduct !== 'premium' && this.state.loggedInUser.userProduct !== '' &&
+          <h2>
+            Sorry, this application requires you to be a premium user only. I am currently working on something for non premium users!
+          </h2>
         }
 
-        { this.state.loggedIn && 
-          <button onClick={() => this.postPlaylist(this.state.loggedInUser.userId, this.state.favoriteArtistsSongs.songs)}>
-            Post Playlist
-          </button>
+        { this.state.loggedIn && this.state.loggedInUser.userProduct === 'premium' &&
+          <div id="modal">
+            <h1>Welcome to Marvify!</h1>
+            <p>Hello, Spotify User! Thank you for taking the time to play my game. This is a 10 question quiz to see how well you know your favorite artist's top songs!</p>
+            <p>As soon as you click 'play', your spotify account will immediately create a playlist of your top artist's favorite songs. You will be prompted 10 questions, one by one, 
+              giving you ten seconds to determine the song. If you guess incorrectly or run out of time, the question will
+              be flagged as incorrect. Otherwise, it will be correct.
+            </p>
+
+            <p>
+              PLEASE NOTE: You must take this quiz in one sitting! If you attempt to click away from the screen or open up your playlist, you will automatically score a 0/10.
+              Additionally, if you attempt to pause the song in the middle of any question, that question will be marked as wrong.
+            </p>
+            <p>Once you are finished, you will receive your results. Good luck!</p>
+
+            <button onClick={() => this.startGame()}>
+              PLAY NOW!
+            </button>
+          </div>
         }
+
       </div>
     )
   }
