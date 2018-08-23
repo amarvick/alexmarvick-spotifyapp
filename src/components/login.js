5fd35a4506460ebaffda5b7a72ad378855be21b1
Alex Marvick <alex@fiduciarytech.com>
2018-08-23 08:29:53 -0700

    Localhost:8888

--- src/components/login.js
+++ src/components/login.js
@@ -5,7 +5,7 @@ class Login extends Component {
   render() {
     return (
       <div className='App'>
-        <a href='https://am-spotify-quiz-api.herokuapp.com/login'>Login to Spotify</a>
+        <a href='http://localhost:8888/login'>Login to Spotify</a>
       </div>
     )
   }
