88934743aa96aadbf90d1335fe5b8fe82ed6425c
Alex Marvick <alex@fiduciarytech.com>
2018-09-10 21:11:30 -0700

    Reuploading

--- src/components/login.js
+++ src/components/login.js
@@ -11,7 +11,7 @@ class Login extends Component {
       <div className='App container container-fluid'>
         <h1 className="display-4" style = {style}>WELCOME TO MARVIFY!</h1>
         <p className="lead">PREMIUM USERS! Are you REALLY as big of a fan as your favorite music group?! Find out now!</p>
-        <a className="btn btn-success" href='https://am-spotify-quiz-api.herokuapp.com/login'>Login to Spotify</a>  {/*'http://localhost:8888/login'>Login to Spotify</a> */}
+        <a className="btn btn-success" href='https://am-spotify-quiz-api.herokuapp.com/login'>Login to Spotify </a>  {/*'http://localhost:8888/login'>Login to Spotify</a> */}
       </div>
     )
   }
