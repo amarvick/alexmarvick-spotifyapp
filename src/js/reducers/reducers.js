e0eb1bb872284fa8d18254f1fa64e5595095d781
Alex Marvick <amarvick94@gmail.com>
2018-09-17 11:51:49 -0700

    Commenting reducers.js

--- src/js/reducers/reducers.js
+++ src/js/reducers/reducers.js
@@ -1,3 +1,8 @@
+/* File Name: reducers.js                                           *
+ * Description: Combines all reducers together so they are all in a *
+ *              single package as opposed to getting only a few at  *
+ *              a time for each component                           */
+
 import { combineReducers } from 'redux'
 
 import user from './userReducer'
